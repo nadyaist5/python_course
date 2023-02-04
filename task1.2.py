987092acdf3e8259416c437161958a886d089432
nadyaist5 <124414000+nadyaist5@users.noreply.github.com>
2023-02-04 18:16:49 +0700

    Update task1.2.py

--- task1.2.py
+++ task1.2.py
@@ -1,14 +1,7 @@
-list = []
+list = [1, 2, 3, 1, 4]
+setlist = set(list)
 
-def unique(list):
-  count = 0
-  for i in range(len(list)):
-    for j in range(i+1, len(list)):
-      if list[i] == list[j]:
-        count = count + 1
-  if count > 0:
-    return True
-  else:
-    return False
-
-print(unique([1, 2, 3, 1, 4]))
+if len(list) != len(setlist):
+  print(True)
+else: 
+  print(False)
