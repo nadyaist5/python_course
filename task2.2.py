074d1ab51820d842d5f1897b82aeb550bbd71633
nadyaist5 <124414000+nadyaist5@users.noreply.github.com>
2023-02-19 01:15:11 +0700

    Update task2.2.py

--- task2.2.py
+++ task2.2.py
@@ -1,17 +1,12 @@
 import csv
 
-data = []
 with open('stage3_test.csv', 'r', encoding='utf8') as f:
     reader = csv.reader(f)
-    for row in reader:
-        if (row[4] == "Price"):
-            data.append(row)
-            continue
-        if (float(row[4]) > 10000) and (float(row[4]) <= 50000):
-            data.append(row)
-            print(row[4])
-
-with open('normal_price.csv', 'w', encoding='utf8') as f:
-    writer = csv.writer(f)
-    for row in data:
-        writer.writerow(row)
+    with open('normal_price.csv', 'w', encoding='utf8') as g:
+        writer = csv.writer(g)
+        for row in reader:
+            if (row[4] == "Price"):
+                writer.writerow(row)
+                continue
+            if 10000 < float(row[4]) <= 50000:
+                writer.writerow(row)
