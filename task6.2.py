6dd323ae190e5a43dcc56294a8d9138553a3bc2b
nadyaist5 <124414000+nadyaist5@users.noreply.github.com>
2023-03-17 22:41:14 +0700

    Update task6.2.py

--- task6.2.py
+++ task6.2.py
@@ -1,5 +1,4 @@
 import time
-import os
 
 t = time.time()
 sum = 0
@@ -7,6 +6,5 @@ for i in range(1000000):
     sum += i
 secs = time.time() - t
 
-with open("f.txt", "w") as f:
+with open(f"{time.strftime('%H.%M.%S_%d.%m.%Y')}.txt", "w") as f:
     f.write(str(secs))
-os.rename("f.txt", f"{time.strftime('%H.%M.%S_%d.%m.%Y')}.txt")
